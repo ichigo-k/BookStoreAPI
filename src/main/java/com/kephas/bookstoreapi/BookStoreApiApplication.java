37b6e5e9e73b6cd6e4c272a46ebe32d11dc80e51
tette <tettehsaintluke@gmail.com>
2025-09-09 08:39:10 +0000

    Review

--- src/main/java/com/kephas/bookstoreapi/BookStoreApiApplication.java
+++ src/main/java/com/kephas/bookstoreapi/BookStoreApiApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @SpringBootApplication
-@EnableMethodSecurity
 public class BookStoreApiApplication {
 
     public static void main(String[] args) {
